8d960f6f129ef01dac851ced023454ff915cd17a
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-07-27 12:12:15 -0700

    built <Subtotal />

---
+++ src/Checkout.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useStateValue } from './StateProvider';
+import './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
+import './CheckoutProduct.css';
+import Subtotal from './Subtotal';
+
+
+function Checkout() {
+  const [{ basket }] = useStateValue();
+
+  return (
+    <div className="checkout">
+      <div className="checkout_left"> 
+        <img 
+          className="checkout_ad" 
+          src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZmZkNTUwNjEt/ZmZkNTUwNjEt-NTFjZDdmN2Et-w3000._CB409612199_.jpg"
+          alt="" 
+        />
+        {basket?.length === 0 ? (
+          <div>
+            <h2>Your Shopping Basket is empty</h2>
+            <p>
+              You have no items in your basket. To buy one or more itmes click, 
+              "Add to basket" next to the item.
+            </p>
+          </div>
+          ) : (
+            <div>
+              <h2 className="checkout_title">Your Shopping Basket</h2>
+                {/* List out all of the Checkout Products */}
+                {basket.map(item => (
+                  <CheckoutProduct 
+                    id={item.id}
+                    title={item.title}
+                    image={item.image}
+                    price={item.price}
+                    rating={item.rating}
+                  />
+                ))}
+                
+            </div>
+          )}
+        </div>
+        {basket.length > 0 && (
+          <div className="checkout_right">
+            <Subtotal />
+          </div>
+        )}
+      </div>
+    );
+  }
+
+export default Checkout;
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,29 @@
 export const initialState = {
-  basket: [],
+  basket: [
+  {
+    id: "3254354345",
+    title: 
+      "New Apple iPad Pro (12.9-inch, Wi-fi, 128GB) - Space Gray (4th Generation)",
+    price: 598.99,
+    rating: 4,
+    image: 
+      "https://images-na.ssl-images-amazon.com/images/I/81SGb5l%2BlZL._AC_SX679_.jpg"
+  },
+  {
+    id: "3254354345",
+    title: 
+      "New Apple iPad Pro (12.9-inch, Wi-fi, 128GB) - Space Gray (4th Generation)",
+    price: 598.99,
+    rating: 4,
+    image: 
+      "https://images-na.ssl-images-amazon.com/images/I/81SGb5l%2BlZL._AC_SX679_.jpg"
+  }
+  ],
   user: null,
 };
+
+export const getBasketTotal = (basket) => 
+basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) =>{
   console.log(action);
@@ -13,7 +35,22 @@ const reducer = (state, action) =>{
       };
       // Logic for adding item to basket
     case 'REMOVE_FROM_BASKET':
-      return { state };
+      let newBasket = [...state.basket];
+
+      const index = state.basket.findIndex((basketItem) => basketItem.id === action.id)
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in basket`
+        );
+      }
+
+      return { 
+        ...state, 
+        basket: newBasket 
+      };
       // Logic for Removing item from basket
     default:
       return state;
